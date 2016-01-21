b78ae777f012178499a19d272089b02316ef9efb
SH00KS <Rob@robs-mac-pro.freshwatergroup.dom>
2016-01-21 16:04:54 +0000

    Some CSS styling & error handling
    Added some CSS style to it and began doing event handling for the first
    time.
    
    /* Note */
    Probably the least fun part of programming so far.

--- abv.js
+++ abv.js
@@ -8,23 +8,60 @@
 	var roundedAbv = parseFloat(abv).toFixed(2); 
 
 	var abvResult = document.getElementById('ABVResult');
-	abvResult.innerHTML = roundedAbv + " %";
+	abvResult.innerHTML = "Alcohol by Volume (ABV):" + roundedAbv + " %";
 
 	//Maths equation for calories
 	var calories = 26.40 * abv * 1.6;
 	var roundedCalories = parseFloat(calories).toFixed(2); 
 
 	var caloriesResult = document.getElementById('caloriesResult');
-	caloriesResult.innerHTML = roundedCalories;
+	caloriesResult.innerHTML = "Calories per bottle (based on 750ml):" + roundedCalories;
 
 	var alertBox = document.getElementById('alertBox');
 
 		if (finalGravity > originalGravity) {
 			alertBox.innerHTML = ('The final gravity should be lower than the original gravity');
-			return false;
+		}
+
+		if (!originalGravity || !finalGravity){
+			alertBox.innerHTML = ('Please fill in all of the fields');
+		}
+
+		if (isNaN(finalGravity || originalGravity)){
+			alertBox.innerHTML = ('Only numbers allowed sorry.');
 		}
 
 	}
 
-	document.getElementById("calculateButton").onclick = calculateAlcoholByVolume;
+	
+
+
+function validateInput(e){
+	var originalGravity = document.getElementById('originalGravityText').value;
+	var finalGravity = document.getElementById('finalGravityText').value;
+
+	if (finalGravity > originalGravity) {
+			alertBox.innerHTML = ('The final gravity should be lower than the original gravity');
+			return false;
+		}
+
+		if (!originalGravity || !finalGravity){
+			alertBox.innerHTML = ('Please fill in all of the fields');
+			return false;
+		}
+
+		if (isNaN(finalGravity || originalGravity)){
+			alertBox.innerHTML = ('Only numbers allowed sorry.');
+			return false;
+		}
+
+		if (booleanValue == true){
+			alert('hi')
+		}
+
+}
+
+
+document.getElementById("calculateButton").onclick = calculateAlcoholByVolume;
+	
 	
