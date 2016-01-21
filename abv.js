481d1b13de8e7abdd2b0f98b14dc4f56253ffeaa
SH00KS <Rob@robs-mac-pro.freshwatergroup.dom>
2016-01-21 11:57:20 +0000

    Error message added
    Added an error message if the user enters a higher final gravity
    reading than an original gravity reading. This is impossible.
    
    Still allows a calculation to be rendered, must work on stopping this.

--- abv.js
+++ abv.js
@@ -3,13 +3,27 @@
 	var originalGravity = document.getElementById('originalGravityText').value;
 	var finalGravity = document.getElementById('finalGravityText').value;
 	
-	//Maths equation
+	//Maths equation for ABV
 	var abv = (1.05/0.79) * ((originalGravity - finalGravity) / finalGravity) * 100;
 	var roundedAbv = parseFloat(abv).toFixed(2); 
 
-	//alert(math);
-	var result = document.getElementById('ABVResult');
-	result.innerHTML = roundedAbv + " %";
+	var abvResult = document.getElementById('ABVResult');
+	abvResult.innerHTML = roundedAbv + " %";
+
+	//Maths equation for calories
+	var calories = 26.40 * abv * 1.6;
+	var roundedCalories = parseFloat(calories).toFixed(2); 
+
+	var caloriesResult = document.getElementById('caloriesResult');
+	caloriesResult.innerHTML = roundedCalories;
+
+	var alertBox = document.getElementById('alertBox');
+
+		if (finalGravity > originalGravity) {
+			alertBox.innerHTML = ('The final gravity should be lower than the original gravity');
+			return false;
+		}
+
 	}
 
 	document.getElementById("calculateButton").onclick = calculateAlcoholByVolume;
