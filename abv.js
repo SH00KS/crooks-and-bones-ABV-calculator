0d92e7a9967016bc22810116fcfa78b2df23cd86
SH00KS <Rob@robs-mac-pro.freshwatergroup.dom>
2016-01-22 11:25:51 +0000

    CSS styling
    Bit more styling and fix of error handling.

--- abv.js
+++ abv.js
@@ -1,66 +1,55 @@
 
 	function calculateAlcoholByVolume(e) {
-	var originalGravity = document.getElementById('originalGravityText').value;
-	var finalGravity = document.getElementById('finalGravityText').value;
+  
+	  var result =  validateInput();
+	  
+	  
+		var originalGravity = document.getElementById('originalGravityText').value;
+		var finalGravity = document.getElementById('finalGravityText').value;
+		
+		//Maths equation for ABV
+		var abv = (1.05/0.79) * ((originalGravity - finalGravity) / finalGravity) * 100;
+		var roundedAbv = parseFloat(abv).toFixed(2); 
+
+		var abvResult = document.getElementById('ABVResult');
+		abvResult.innerHTML = "<span class='resultsLabel'>Alcohol by Volume (ABV): <br></span>" + roundedAbv + " %";
+
+		//Maths equation for calories
+		var calories = 26.40 * abv * 1.6;
+		var roundedCalories = parseFloat(calories).toFixed(2); 
+
+		var caloriesResult = document.getElementById('caloriesResult');
+		caloriesResult.innerHTML = "<span class='resultsLabel'>Calories per bottle (based on 750ml): <br></span>" + roundedCalories;
+
+		var alertBox = document.getElementById('alertBox');
 	
-	//Maths equation for ABV
-	var abv = (1.05/0.79) * ((originalGravity - finalGravity) / finalGravity) * 100;
-	var roundedAbv = parseFloat(abv).toFixed(2); 
-
-	var abvResult = document.getElementById('ABVResult');
-	abvResult.innerHTML = "Alcohol by Volume (ABV):" + roundedAbv + " %";
-
-	//Maths equation for calories
-	var calories = 26.40 * abv * 1.6;
-	var roundedCalories = parseFloat(calories).toFixed(2); 
-
-	var caloriesResult = document.getElementById('caloriesResult');
-	caloriesResult.innerHTML = "Calories per bottle (based on 750ml):" + roundedCalories;
-
-	var alertBox = document.getElementById('alertBox');
-
-		if (finalGravity > originalGravity) {
-			alertBox.innerHTML = ('The final gravity should be lower than the original gravity');
-		}
-
-		if (!originalGravity || !finalGravity){
-			alertBox.innerHTML = ('Please fill in all of the fields');
-		}
-
-		if (isNaN(finalGravity || originalGravity)){
-			alertBox.innerHTML = ('Only numbers allowed sorry.');
-		}
-
 	}
 
-	
+	function validateInput(){
+		
+		var originalGravity = document.getElementById('originalGravityText').value;
+		var finalGravity = document.getElementById('finalGravityText').value;
+		var alertBox = document.getElementById('alertBox');
 
-
-function validateInput(e){
-	var originalGravity = document.getElementById('originalGravityText').value;
-	var finalGravity = document.getElementById('finalGravityText').value;
-
-	if (finalGravity > originalGravity) {
-			alertBox.innerHTML = ('The final gravity should be lower than the original gravity');
-			return false;
-		}
-
-		if (!originalGravity || !finalGravity){
-			alertBox.innerHTML = ('Please fill in all of the fields');
-			return false;
-		}
-
-		if (isNaN(finalGravity || originalGravity)){
-			alertBox.innerHTML = ('Only numbers allowed sorry.');
-			return false;
-		}
-
-		if (booleanValue == true){
-			alert('hi')
-		}
-
-}
-
+    
+    	if (!originalGravity || !finalGravity){
+				alertBox.innerHTML = ('Please fill in all of the fields.');
+				return false;
+			}
+      
+      	if (isNaN(finalGravity) || isNaN(originalGravity)){
+				alertBox.innerHTML = ('Only numbers allowed sorry.');
+				return false;
+			}
+		
+		if (finalGravity > originalGravity) {
+				alertBox.innerHTML = ('The final gravity should be lower than the original gravity.');
+				return false;
+			}
+			
+				alertBox.innerHTML = ('');
+			return true;
+	}
 
 document.getElementById("calculateButton").onclick = calculateAlcoholByVolume;
 	
